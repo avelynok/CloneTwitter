eb29f4559b5c546ae8df33be914ed77d292c7ac3
Tiffany McLean <tamclean324@gmail.com>
2019-12-19 23:05:11 -0500

    corrects message feed link

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -1,9 +1,11 @@
 import Home from "./Home";
 import Profile from "./Profile";
 import NotFound from "./NotFound";
+import MessageFeed from "./MessageFeedgit "
 
 export default {
   Home: { path: "/", component: Home },
   Profile: { path: "/profile/:username", component: Profile },
+  MessageFeed: { path: "/messagefeed", component: MessageFeed },
   NotFound: { path: "*", component: NotFound }
 };
