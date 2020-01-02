19cae3f1b7c2c8a883ff1c43e53205605cd56f03
Tiffany McLean <tamclean324@gmail.com>
2020-01-01 23:27:26 -0500

    adds style to navbar and other components

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -3,9 +3,7 @@ import Profile from "./Profile";
 import NotFound from "./NotFound";
 import MessageFeed from "./MessageFeed";
 import CreateUser from "./CreateUser";
-import UpdateUser from "./UpdateUser";
 import HomePage from "./HomePage";
-
 
 export default {
   Home: { path: "/", component: Home },
@@ -13,6 +11,6 @@ export default {
   Profile: { path: "/profile/:username", component: Profile },
   MessageFeed: { path: "/messagefeed", component: MessageFeed },
   CreateUser: { path: "/createuser", component: CreateUser },
-  UpdateUser: { path: "/updateuser", component: UpdateUser },
+
   NotFound: { path: "*", component: NotFound }
 };
