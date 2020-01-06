dd55049a08156e97060b61f8cc27be8fe84ec9ed
Tiffany McLean <tamclean324@gmail.com>
2020-01-05 21:52:28 -0500

    adds UI for the create user profie

--- src/react/components/CreateUserProfileForm.js
+++ src/react/components/CreateUserProfileForm.js
@@ -1,23 +1,52 @@
 import React, { Component } from "react";
-import { Form, Button } from "../components";
+import { Form, Button, Input, Checkbox } from "../components";
+import './CreateUserProfileForm.css'
+
 
 class CreateUserProfileForm extends Component {
   render() {
     return (
-      <Form>
-        <Form.Field>
-          <label>Username</label>
-          <input placeholder="Enter your username" />
+      <Form >
+        <Form.Group widths="equal">
+          <Form.Field
+            control={Input}
+            label="Username"
+            placeholder="Enter your username"
+            className = 'username'
+          />
+          
+          <Form.Field
+            control={Input}
+            label="Display Name"
+            placeholder="Enter the name you want displayed"
+           
+          />
+
+          <Form.Field
+            control={Input}
+            label="Password"
+            type="password"
+            placeholder="Enter your password"
+            className = 'password'
+          >
+          </Form.Field>
+          <Form.Field
+            control={Input}
+            label="Password"
+            type="password"
+            placeholder="Confirm Your Password"
+            className = 'password'
+          >
+          </Form.Field>
+        </Form.Group>
+
+        <Form.Field
+          control={Checkbox}
+          label="I agree to the Terms and Conditions"
+        />
+        <Form.Field control={Button} color="grey">
+          Submit
         </Form.Field>
-        <Form.Field>
-          <label>Display Name</label>
-          <input placeholder= "Enter the name that you want displayed"/>
-        </Form.Field>
-        <Form.Field>
-          <label>Password</label>
-          <input placeholder="Enter your password" />
-        </Form.Field>
-        <Button type="submit">Register Your Account</Button>
       </Form>
     );
   }
